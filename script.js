369d6f1878a69256ccc2ab4b0cce1a204e43f69b
Dhairya Shukla <113496448+dhairyashukla08@users.noreply.github.com>
2025-11-29 16:13:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
-  // your code here
+  const firstSpaceIndex = s.indexOf(' ');
+	if (firstSpaceIndex === -1){
+		return s;
+	}
+	else{
+		return s.substring(0, firstSpaceIndex);
+	}
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
